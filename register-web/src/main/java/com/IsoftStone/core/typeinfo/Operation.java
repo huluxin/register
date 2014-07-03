12c2f4a323038306452ca11f4bff2080190c09d4
RunGen's <wangyj0898@126.com>
2014-07-03 23:08:51 +0800

    JavaCore 类型信息 命令模式

---
+++ register-web/src/main/java/com/IsoftStone/core/typeinfo/Operation.java
@@ -0,0 +1,10 @@
+package com.IsoftStone.core.typeinfo;
+
+/**
+ * 命令模式
+ * Created by Bachelor Wang on 2014/7/2.
+ */
+public interface Operation {
+    String description();
+    void command();
+}
