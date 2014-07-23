3a5eaaf2d5afbfd639449375e1d703b3a5c51798
wangyj <wangyj0898@126.com>
2014-07-23 18:13:20 +0800

    effective java ch02

---
+++ register-web/src/main/java/com/IsoftStone/effective/ch02/Service.java
@@ -0,0 +1,9 @@
+package com.IsoftStone.effective.ch02;
+
+/**
+ * Sevice interface
+ * Created by Administrator on 2014/7/23.
+ */
+public interface Service {
+
+}
---
+++ register-web/src/main/java/com/IsoftStone/effective/ch02/Services.java
@@ -0,0 +1,32 @@
+package com.IsoftStone.effective.ch02;
+
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by Administrator on 2014/7/23.
+ */
+public class Services {
+    private Services(){}
+
+    private static final Map<String,Provider> providers = new ConcurrentHashMap<String,Provider>() ;
+    public static final String DEFAULT_PROVIDER_NAME = "<def>";
+
+    public static void registerDefaultProvider(Provider p){
+        registerProvider(DEFAULT_PROVIDER_NAME,p);
+    }
+    public static void registerProvider(String name,Provider p){
+        providers.put(name,p);
+    }
+
+    public static Service newInstance(){
+        return newInstance(DEFAULT_PROVIDER_NAME);
+    }
+    public static Service newInstance(String name){
+        Provider p = providers.get(name);
+        if(p == null)
+            throw new IllegalArgumentException("No provider " + name);
+        return p.newService();
+    }
+}
