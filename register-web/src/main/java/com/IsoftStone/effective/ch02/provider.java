c9cbfcace86ec14912376c958d523a5f935e0eaa
wangyj <wangyj0898@126.com>
2014-07-23 18:14:47 +0800

    effective java ch02

---
+++ register-web/src/main/java/com/IsoftStone/effective/ch02/provider.java
@@ -0,0 +1,9 @@
+package com.IsoftStone.effective.ch02;
+
+/**
+ * Service provider interface
+ * Created by Administrator on 2014/7/23.
+ */
+public interface Provider {
+    Service newService();
+}
