bf827856ed9d80bb7a21a443090c395532ef61c3
RunGen's <wangyj0898@126.com>
2014-08-10 23:46:34 +0800

    javacore 泛型方法 Set 368

--- register-web/src/main/java/com/IsoftStone/core/net/mindview/util/Sets.java
+++ register-web/src/main/java/com/IsoftStone/core/net/mindview/util/Sets.java
@@ -3,25 +3,27 @@ package com.IsoftStone.core.net.mindview.util;
 import java.util.*;
 
 public class Sets {
+    //返回Set ，它将2个参数合并在一起
   public static <T> Set<T> union(Set<T> a, Set<T> b) {
     Set<T> result = new HashSet<T>(a);
     result.addAll(b);
     return result;
   }
+    //返回的Set 只包含2个参数共有部分
   public static <T>
   Set<T> intersection(Set<T> a, Set<T> b) {
     Set<T> result = new HashSet<T>(a);
     result.retainAll(b);
     return result;
   }	
-  // Subtract subset from superset:
+  // Subtract subset from superset: 从 superset 中移除 subset 包含的元素
   public static <T> Set<T>
   difference(Set<T> superset, Set<T> subset) {
     Set<T> result = new HashSet<T>(superset);
     result.removeAll(subset);
     return result;
   }
-  // Reflexive--everything not in the intersection:
+  // Reflexive--everything not in the intersection: 返回的set 包含除了交集之外的所有元素
   public static <T> Set<T> complement(Set<T> a, Set<T> b) {
     return difference(union(a, b), intersection(a, b));
   }
